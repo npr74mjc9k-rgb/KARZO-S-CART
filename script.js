e6ae68f00c4ed6ba8391d380ea3483b419be17d7
npr74mjc9k-rgb <npr74mjc9k@privaterelay.appleid.com>
2026-08-14 21:19:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
    SUPABASE
 ========================== */
 
-const SUPABASE_URL = "https://efqfljpruddxafhyzvod.supabase.co;
+const SUPABASE_URL = "https://efqfljpruddxafhyzvod.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_idVrzmCyqarPHKefeZLg4Q_ntaeSLz6";
 
